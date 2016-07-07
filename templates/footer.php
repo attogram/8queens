915bfcd2cbc5722aa07bb503593741b25e31e372
Attogram <barberi.office@gmail.com>
2016-07-07 10:30:20 +0200

    versioning, cleaning

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,11 @@
-<?php
-// Attogram - template - footer
-
+<?php // 8queens - footer v0.0.1
 ?>
-<footer><div class="footer">
-<nobr><a href="<?php print $this->path; ?>/">Play 8 Queens</a>
+<footer>
+<div class="footer">
+<nobr><a href="<?php print $this->path; ?>/">Play 8 Queens</a></nobr>
 <nobr> &nbsp;|&nbsp; <a href="<?php print $this->path; ?>/about/">About the 8 Queens Puzzle</a></nobr>
 <nobr> &nbsp;|&nbsp; <a href="<?php print $this->path; ?>/solutions/">Finding solutions in PHP</a></nobr>
 <nobr> &nbsp;|&nbsp; <a href="<?php print $this->path; ?>/92/">The 92 solutions</a></nobr>
-<br />
-<nobr> &nbsp;|&nbsp; Served to <?php print @$_SERVER['REMOTE_ADDR']; ?> @ <?php print gmdate('Y-m-d H:i:s'); ?> UTC</nobr>
-</div></footer>
+</div>
+</footer>
 </body></html>
--- templates/header.php
+++ templates/header.php
@@ -1,11 +1,12 @@
-<?php
-// Attogram - templates - header
+<?php // 8queens - header v0.0.1
 
-if( !isset($title) || !$title ) { $title = 'Play 8 Queens'; }
+
+if( !isset($title) || !$title || !is_string($title) ) {
+  $title = 'Play 8 Queens';
+}
 
 ?><!doctype html><html><head>
 <title><?php print $title; ?></title>
 <meta charset="utf-8" />
 <link rel="stylesheet" type="text/css" href="<?php print $this->path; ?>/web/css.css">
 </head><body>
-<?php

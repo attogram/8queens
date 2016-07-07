6db299fb84af462870daaf9db8d63a182671ee7b
Attogram <barberi.office@gmail.com>
2016-07-07 10:49:06 +0200

    fork me on github ribbon

--- actions/home.php
+++ actions/home.php
@@ -7,6 +7,8 @@ $this->page_header('Play 8 Queens - the classic chess puzzle')
 <script src="<?php print $this->path; ?>/web/cjs030/js/jquery-1.10.1.min.js"></script>
 <script src="<?php print $this->path; ?>/web/cjs030/js/chessboard-0.3.0.js"></script>
 
+<a href="https://github.com/attogram/8queens"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
+
 <table border="0" cellpadding="0" cellspacing="0" style="margin: 0px auto;">
  <tr>
   <td valign="top" style="padding:10px 20px 0px 30px;">

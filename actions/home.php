d570f707400410588197a233ddc9084f396b2c6f
Attogram <barberi.office@gmail.com>
2016-07-13 00:07:37 +0200

    rm banner

--- actions/home.php
+++ actions/home.php
@@ -1,4 +1,4 @@
-<?php // 8queens - Home page v0.0.2
+<?php // 8queens - Home page v0.0.3
 
 $this->pageHeader('Play 8 Queens - the classic chess puzzle')
 
@@ -6,8 +6,6 @@ $this->pageHeader('Play 8 Queens - the classic chess puzzle')
 <link rel="stylesheet" href="<?php print $this->path; ?>/web/cjs030/css/chessboard-0.3.0.css" />
 <script src="<?php print $this->path; ?>/web/cjs030/js/jquery-1.10.1.min.js"></script>
 <script src="<?php print $this->path; ?>/web/cjs030/js/chessboard-0.3.0.js"></script>
-
-<a href="https://github.com/attogram/8queens"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
 
 <table border="0" cellpadding="0" cellspacing="0" style="margin: 0px auto;">
  <tr>

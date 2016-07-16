3105387ce7b765c0b14abc73008e6967113b9d9e
Attogram <barberi.office@gmail.com>
2016-07-16 21:50:40 +0200

    testToBeWrittenRealSoonNow

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -1,14 +1,14 @@
 <?php
-// Attogram Framework - 8queens Module - Base Test v0.0.1
+// Attogram Framework - 8queens Module - Base Test v0.0.2
 
 use PHPUnit\Framework\TestCase;
 
 class BaseTest extends PHPUnit\Framework\TestCase
 {
 
-    public function testTruthiness()
+    public function testToBeWrittenRealSoonNow()
     {
-        $this->assertTrue( true, 'true not true!' );
+        $this->assertTrue(true, 'true not true!');
     }
 
 }
